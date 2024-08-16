1c7e604d6644fffe21d83d32d2b68a205f5031e1
hadjyahia douaa <hadjyahiadouaa04@gmail.com>
2024-08-16 11:00:14 +0200

    le fichier connexion est ajouter

---
+++ src/Models/Connexion.java
@@ -0,0 +1,13 @@
+
+package Models;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Connexion {
+    
+    public static Connection connecter () throws SQLException{
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/gestionstock","root","DouaaMySQL2004"); 
+    }  
+}
